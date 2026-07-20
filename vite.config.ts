51f500aecaf0a08b869ac88dce4f25e518bbae2d
ShotByAkim-Photography-and-Videography <citation.unitedcoveringllc@gmail.com>
2026-07-20 22:32:10 +0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig} from 'vite';
+
+export default defineConfig(() => {
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+          about: path.resolve(__dirname, 'about.html'),
+          services: path.resolve(__dirname, 'services.html'),
+          contact: path.resolve(__dirname, 'contact.html'),
+          privacy: path.resolve(__dirname, 'privacy-policy.html'),
+          terms: path.resolve(__dirname, 'terms-and-conditions.html'),
+        }
+      }
+    }
+  };
+});
